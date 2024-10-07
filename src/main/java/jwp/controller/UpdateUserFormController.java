2906265598d78c4454f569b16ee8c60de9252666
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-07 19:49:39 +0900

    feat: 개인정보 수정 보안 강화 구현

--- src/main/java/jwp/controller/UpdateUserFormController.java
+++ src/main/java/jwp/controller/UpdateUserFormController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
@@ -16,13 +17,18 @@ public class UpdateUserFormController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userId = req.getParameter("userId");
+        String userId = req.getParameter("userId");         // 수정되는 user
         User user = MemoryUserRepository.getInstance().findUserById(userId);
-        if (user != null) {
-            req.setAttribute("user", user);
-            RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
-            rd.forward(req, resp);
-            return;
+
+        HttpSession session = req.getSession();                    // 수정하는 user
+        Object value = session.getAttribute("user");
+
+        if (user != null && value != null) {
+            if (user.equals(value)){            // 수정되는 user와 수정하는 user가 동일한 경우
+                RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+                rd.forward(req, resp);
+                return;
+            }
         }
         resp.sendRedirect("/");
     }
